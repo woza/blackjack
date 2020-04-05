7c9896b7d05a3f680f7f772479a3f1482e327e3d
woza <woza@linux-ehhp.warren.local>
2020-04-05 20:37:21 +1000

    Fixed config file parsing, support setting server parameters therein

--- src/config.py
+++ src/config.py
@@ -11,27 +11,37 @@ class config:
             'video' : 'mplayer',
             'audio' : 'mplayer'
         }
-
-        if path is not None:
-            self.parse(path)
+        self.server = None
+        self.port = '8200'
+        self.parse(path)
 
         self.handlers = {}
         for media_type,handler in self.handler_map.items():
             self.handlers[media_type] = self.handler_factory[handler]
 
+        if self.server is None:
+            print ("Server address not found - aborting")
+            sys.exit(1)
+
     def parse( self, path ):
         conf = configparser.ConfigParser()
         conf.read(path)
 
+        if 'network' in conf.sections():
+            for key,value in conf['network'].items():
+                if key == 'server':
+                    self.server = value
+                if key == 'port':
+                    self.port = value
+
         if 'handlers' in conf.sections():
             for key,value in conf['handlers'].items():
-                if value not in self.handler_factory():
+                if value not in self.handler_factory:
                     print ("Unknown handler '%s' specified for media "\
                            "type '%s'" % (value,key))
                     sys.exit(1)
                 self.handler_map[key] = value
 
         for media_type,handler in self.handler_map.items():
-            if handler in self.sections():
-                self.handler_factory[handler].reconfigure(config[handler])
-    
+            if handler in conf.sections():
+                self.handler_factory[handler].reconfigure(conf[handler])
